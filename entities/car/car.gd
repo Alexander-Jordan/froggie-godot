acde09d045a3e02ffaf0f5f8a9a603fc2e21c319
Alexander-Jordan <curtjordan93@gmail.com>
2025-04-23 10:20:55 +0200

    Add cars and adjust boundaries and frog a bit

---
+++ entities/car/car.gd
@@ -0,0 +1,14 @@
+class_name Car extends Node2D
+
+@export var speed: int = 100
+
+@onready var spawnable_2d: Spawnable2D = $Spawnable2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+
+func _ready() -> void:
+	spawnable_2d.spawned.connect(func(_spawn_position: Vector2): sprite_2d.look_at(position + spawnable_2d.direction))
+	visible_on_screen_notifier_2d.screen_exited.connect(func(): spawnable_2d.call_deferred('despawn'))
+
+func _process(delta: float) -> void:
+	position += spawnable_2d.direction.normalized() * speed * delta
--- entities/player/frog.gd
+++ entities/player/frog.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+@export var spawn_position: Vector2 = Vector2.ZERO
+
+@onready var destructable_2d: Destructable2D = $Destructable2D
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
 @onready var sprite_2d: Sprite2D = $sprite_2d_parent/Sprite2D
 @onready var sprite_2d_parent: Node2D = $sprite_2d_parent
 
-var animation_speed: int = 3
+var animation_speed: int = 8
 var directions: Dictionary[String, Vector2] = {
 	'up': Vector2.UP,
 	'down': Vector2.DOWN,
@@ -13,6 +16,12 @@ var directions: Dictionary[String, Vector2] = {
 }
 var is_moving: bool = false
 var tile_size: int = 16
+var tween: Tween = null
+
+func _ready() -> void:
+	spawn_position = position
+	
+	destructable_2d.destroyed.connect(reset)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if is_moving:
@@ -25,7 +34,7 @@ func move(direction: String) -> void:
 	ray_cast_2d.target_position = directions[direction] * tile_size
 	ray_cast_2d.force_raycast_update()
 	if !ray_cast_2d.is_colliding():
-		var tween: Tween = create_tween()
+		tween = create_tween()
 		tween.tween_property(
 			self,
 			'position',
@@ -39,5 +48,15 @@ func move(direction: String) -> void:
 		
 		await tween.finished
 		
+		tween = null
 		is_moving = false
 		sprite_2d.frame -= 1
+
+func reset() -> void:
+	if tween != null:
+		tween.kill()
+		tween = null
+		sprite_2d.frame = 0
+		is_moving = false
+	position = spawn_position
+	sprite_2d_parent.look_at(self.position + directions.up)
